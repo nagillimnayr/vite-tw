08f86fb244852e6ddb7897f69640d0bb2f02ca78
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-08 21:40:44 -0700

    build: install and configure tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 import Node from './types/Node';
 
-const App = ({children}: {children?: Node})=>{
-  return(
-    <>
-    {children ?? null}
-    </>
-  )
-}
+const App = ({ children }: { children?: Node }) => {
+  return (
+    <main className="">
+      <div>{children ?? null}</div>
+    </main>
+  );
+};
 
 export default App;
