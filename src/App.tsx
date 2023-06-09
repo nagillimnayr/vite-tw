77d87385bb878672d1f5c498a514cadb30f9e2c8
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-08 21:42:25 -0700

    feat: add header to App

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Node from './types/Node';
 const App = ({ children }: { children?: Node }) => {
   return (
     <main className="">
+      <header>
+        <h1>Header</h1>
+      </header>
+
       <div>{children ?? null}</div>
     </main>
   );
